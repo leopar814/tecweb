a3a91d249ba9c29ab2879e79ad625bf958e8aa5c
leopar <leonardo.avalos.080104@gmail.com>
2025-09-25 12:28:21 -0600

    p07: hecho, ejercicio3 && ejercicio4

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -9,7 +9,7 @@
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
-        require 'src/funciones.php';
+        require_once 'src/funciones.php';
         esMultiploDe5y7();
     ?>
     <hr>
@@ -17,14 +17,59 @@
     <h2>Ejercicio 2</h2>
     <p>
         Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una secuencia compuesta por: <br><br>
-        <span><b style="color:blue;">impar</b> <b style="color:red;">par</b> <b style="color:blue;">impar</b></span>
+        <span><b style="color:blue">impar</b> <b style="color:red">par</b> <b style="color:blue">impar</b></span>
     </p>
     <?php
-        secuencia3Numeros();
+        // Agregar botón
+        secuenciaDe3Numeros();
     ?>
     <hr>
 
     <h2>Ejercicio 3</h2>
+    <p>
+        Encontrar el primero número entero obtenido aleatoriamente pero que además sea múltiplo de un 
+        número dado
+    </p>
+    <?php
+        // echo "<p>Con While</p>";
+        multiploDeNumeroDado();
+        // echo "<p>Con Do-While</p>";
+        // multiploDeNumeroDado_doWhile();
+    ?>
+    <hr>
+
+    <h2>Ejercicio 4</h2>
+    <p>
+        Crear un arreglo cuyos índices van de 97 a 122 y cuyos valores son las letras de la ‘a’
+        a la ‘z’. Usa la función chr(n) que devuelve el caracter cuyo código ASCII es n para poner
+        el valor en cada índice
+    </p>
+    <table 
+        border="1" cellspacing="0" cellpadding="5" 
+        style="text-align:center; vertical-align:middle;"
+    >
+            <tr>
+                <th>índice</th>
+                <th>Valor</th>
+            </tr>
+        <?php
+            $ASCII = codigoASCII();
+            foreach($ASCII as $indice => $caracter) {
+                echo "<tr>";
+                echo "<td>$indice</td>";
+                echo "<td>$caracter</td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>
+    <hr>
+
+    <h2>Ejercicio 5</h2>
+
+
+
+
+
 <!-- 
     <h2>Ejemplo de POST</h2>
     <form action="http://localhost/tecweb/practicas/p07/index.php" method="post">
--- practicas/p07/src/funciones.php
+++ practicas/p07/src/funciones.php
@@ -1,20 +1,23 @@
 <?php 
     // Ejercicio 1
     function esMultiploDe5y7() {
-        if(isset($_GET['numero'])) {
-            $num = $_GET['numero'];
+        if(isset($_GET['numEjer1'])) {
+            $num = $_GET['numEjer1'];
             if ($num%5 == 0 || $num%7 == 0) {
                 echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
             }
             else {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
+                echo "<p style='font-size: 18px'>R= El número $num NO es múltiplo de 5 y 7.</p>";
             }
+        } else {
+            echo '<p><i>Error en GET</i></p>';
+            return;
         }
 
     }
 
     // Ejercicio 2
-    function secuencia3Numeros() {
+    function secuenciaDe3Numeros() {
         $secuencias = []; // matriz de Fx3
         $secuenciaObtenida = false;
         $fila = 0;
@@ -43,6 +46,55 @@
 
         echo "<p>$numerosObtenidos números obtenidos en $fila iteraciones</p>";
 
+    }
+
+    // Ejercicio 3
+    function multiploDeNumeroDado() {
+        if(isset($_GET['numEjer3'])) {
+            $num = $_GET['numEjer3'];
+            echo "<p><b>Número obtenido: $num</b></p>";
+        }
+        else {
+            echo '<p><i>Error en GET</i></p>';
+            return;
+        }
+    
+        $numAleatorio = rand(1, 1000);
+        echo "<p>Número generado: $numAleatorio</p>";
+
+        while ($numAleatorio%$num != 0) {
+            $numAleatorio = rand(1, 1000);
+            echo "<p>Número generado: $numAleatorio</p>";
+        }
+
+        echo "<p style='font-size: 18px'>$numAleatorio <em>SÍ</em> es múltiplo de $num</p>";
+    }
+
+    function multiploDeNumeroDado_doWhile() {
+        if(isset($_GET['numEjer3'])) {
+            $num = $_GET['numEjer3'];
+            echo "<p><b>Número obtenido: $num</b></p>";
+        }
+        else {
+            echo '<p><i>Error en GET</i></p>';
+            return;
+        }
+
+        do {
+            $numAleatorio = rand(1, 1000);
+            echo "<p>Número generado: $numAleatorio</p>";
+        } while ($numAleatorio%$num != 0);
+
+        echo "<p style='font-size: 18px'>$numAleatorio <em>SÍ</em> es múltiplo de $num</p>";
+    }
+
+    function codigoASCII(){
+        $ASCII = []; // Índices de 97 a 122 para caracteres ASCII 
+
+        for($i = 97; $i <= 122; $i++)
+            $ASCII[$i] = chr($i);
+        
+        return $ASCII;
     }
 
 
