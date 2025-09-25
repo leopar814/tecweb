ff8f07258d8a4530aa71324da016750843eb8ee9
leopar <leonardo.avalos.080104@gmail.com>
2025-09-24 21:29:16 -0600

    p07: hecho, ejercicio1

---
+++ practicas/p07/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 7</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    <?php
+        require 'src/funciones.php';
+        esMultiploDe5y7();
+    ?>
+    <hr>
+
+    <h2>Ejercicio 2</h2>
+<!-- 
+    <h2>Ejemplo de POST</h2>
+    <form action="http://localhost/tecweb/practicas/p07/index.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+    <br>
+    <?php
+        // if(isset($_POST["name"]) && isset($_POST["email"]))
+        // {
+        //     echo $_POST["name"];
+        //     echo '<br>';
+        //     echo $_POST["email"];
+        // }
+    ?> -->
+</body>
+</html>
