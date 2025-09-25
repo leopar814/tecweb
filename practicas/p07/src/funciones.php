14107208410c0855cb698b96a2e6fe2f839e35fb
leopar <leonardo.avalos.080104@gmail.com>
2025-09-24 22:13:46 -0600

    p07: prueba ejercicio2

--- practicas/p07/src/funciones.php
+++ practicas/p07/src/funciones.php
@@ -1,4 +1,5 @@
 <?php 
+    // Ejercicio 1
     function esMultiploDe5y7() {
         if(isset($_GET['numero'])) {
             $num = $_GET['numero'];
@@ -9,6 +10,31 @@
                 echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
             }
         }
+
+    }
+
+    // Ejercicio 2
+    function secuencia3Numeros() {
+        $secuencias[][3];
+        $secuenciaObtenida = false;
+        $fila = 1;
+        do {
+            for($i = 0; $i < 3; $i++) {
+                $aleatorio = rand(1, 1000);
+                $secuencias[$fila][$i] = $aleatorio;
+            }
+            
+            // Comprobación impar, par, impar
+            if (($secuencias[$fila][0] % 2) != 0 && 
+                ($secuencias[$fila][1] % 2) == 0 && 
+                ($secuencias[$fila][2] % 2) != 0 
+            ) {
+                $secuenciaObtenida = true;
+            }
+
+            $fila++;
+
+        } while (!$secuenciaObtenida);
 
     }
 
