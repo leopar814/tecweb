50578b541b6bd838f50593c3f44532c5fee673e7
leopar <leonardo.avalos.080104@gmail.com>
2025-09-29 21:11:44 -0600

    p05: xhtml verficado && p08: inicio

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -21,59 +21,68 @@
         echo '<h4>Respuesta:</h4>';   
     
         echo '<ul>';
-        echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
-        echo '<li>$_7var es válida porque inicia con guión bajo.</li>';
-        echo '<li>myvar es inválida porque no tiene el signo de dolar ($).</li>';
-        echo '<li>$myvar es válida porque inicia con una letra.</li>';
-        echo '<li>$var7 es válida porque inicia con una letra.</li>';
-        echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
-        echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
+            echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
+            echo '<li>$_7var es válida porque inicia con guión bajo.</li>';
+            echo '<li>myvar es inválida porque no tiene el signo de dolar ($).</li>';
+            echo '<li>$myvar es válida porque inicia con una letra.</li>';
+            echo '<li>$var7 es válida porque inicia con una letra.</li>';
+            echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
+            echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
         echo '</ul>';
     ?>
-    <hr>
+    <hr />
 
     <h2>Ejercicio 2</h2>
     <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
-    <p> 
-        $a = “ManejadorSQL”;<br>
-        $b = 'MySQL’;<br>
-        $c = &$a;
-    </p>
+    <p>$a = "ManejadorSQL";</p>
+    <p>$b = "MySQL";</p>
+    <p>$c = &amp;$a;</p>
+
     <ol>
-        <li>Ahora muestra el contenido de cada variable</li>
-        <?php
-            $a = "ManejadorSQL";
-            $b = "MySQL";
-            $c = &$a;
+        <li>
+            <p>Ahora muestra el contenido de cada variable</p>
+            <?php
+                $a = "ManejadorSQL";
+                $b = "MySQL";
+                $c = &$a;
 
-            echo "<p>\$a = $a </p>";
-            echo "<p>\$b = $b </p>";
-            echo "<p>\$c = $c </p>";
+                echo "<p>\$a = $a </p>";
+                echo "<p>\$b = $b </p>";
+                echo "<p>\$c = $c </p>";
 
-        ?>
-        <li>Agrega al código actual las siguientes asignaciones:</li>
-        <p> 
-            $a = “PHP server”;<br>
-            $b = &$a;
-        </p>
-        <li>Vuelve a mostrar el contenido de cada uno</li>
-        <?php
-            $a = "PHP server";
-            $b = &$a;
-            echo "<p>\$a = $a </p>";
-            echo "<p>\$b = $b </p>";
-            echo "<p>\$c = $c </p>";
+            ?>
+        </li>
 
-            unset($b, $c); // se elimina variables para evitar futuras referencias 
-        ?>
-        <li>Describe en y muestra en la página obtenida qué ocurrió en el segundo bloque de asignaciones</li>
-        <p>
-            Al establecer la referencia de $b=&$a, hace que que la variable b muestre lo mismo 
-            que la variable a. También, antes se hizo la referencia $c=&$a, provocando el mismo resultado.<br>
-            Por esta razón las 3 variables contienen el mismo valor.
-        </p>
+        <li>
+            <p>Agrega al código actual las siguientes asignaciones:</p>
+            <p> 
+                $a = “PHP server”;<br />
+                $b = &amp;$a;
+            </p>
+        </li>
+
+        <li>
+            <p>Vuelve a mostrar el contenido de cada uno</p>
+            <?php
+                $a = "PHP server";
+                $b = &$a;
+                echo "<p>\$a = $a </p>";
+                echo "<p>\$b = $b </p>";
+                echo "<p>\$c = $c </p>";
+
+                unset($b, $c); // se elimina variables para evitar futuras referencias 
+            ?>
+        </li>
+        <li>
+            <p>Describe en y muestra en la página obtenida qué ocurrió en el segundo bloque de asignaciones</p>
+            <p>
+                Al establecer la referencia de $b=&amp;$a, hace que que la variable b muestre lo mismo 
+                que la variable a. También, antes se hizo la referencia $c=&amp;$a, provocando el mismo resultado.<br />
+                Por esta razón las 3 variables contienen el mismo valor.
+            </p>
+        </li>
     </ol>
-    <hr>
+    <hr />
 
     <h2>Ejercicio 3</h2>
     <p>
@@ -82,12 +91,12 @@
         arreglo):
     </p>
     <p>
-        $a = "PHP5";<br>
-        $z[] = &$a;<br>
-        $b = "5a version de PHP";<br>
-        $c = $b*10;<br>
-        $a .= $b;<br>
-        $b *= $c;<br>
+        $a = "PHP5";<br />
+        $z[] = &amp;$a;<br />
+        $b = "5a version de PHP";<br />
+        $c = $b*10;<br />
+        $a .= $b;<br />
+        $b *= $c;<br />
         $z[0] = "MySQL";        
     </p>
     <?php
@@ -108,7 +117,7 @@
         echo "<li>\$z[0]= $z[0] </li>";
         echo '</ul>';
     ?>
-    <hr>
+    <hr />
 
     <h2>Ejercicio 4</h2>
     <p>
@@ -126,14 +135,14 @@
 
         unset($z)
     ?>
-    <hr>
+    <hr />
 
     <h2>Ejercicio 5</h2>
     <p>Dar el valor de las variables $a, $b, $c al final del siguiente script:</p>
     <p>
-        $a = “7 personas”; <br>
-        $b = (integer) $a; <br>
-        $a = “9E3”;<br>
+        $a = “7 personas”; <br />
+        $b = (integer) $a; <br />
+        $a = “9E3”;<br />
         $c = (double) $a;
     </p>
 
@@ -150,20 +159,20 @@
         echo '</ul>';
 
     ?>
-    <hr>
+    <hr />
 
     <h2>Ejercicio 6</h2>
     <p>
-        $a = “0”;<br>
-        $b = “TRUE”;<br>
-        $c = FALSE;<br>
-        $d = ($a OR $b);<br>
-        $e = ($a AND $c);<br>
+        $a = “0”;<br />
+        $b = “TRUE”;<br />
+        $c = FALSE;<br />
+        $d = ($a OR $b);<br />
+        $e = ($a AND $c);<br />
         $f = ($a XOR $b);
     </p>
     <p>
         Dar y comprobar el valor booleano de las variables $a, $b, $c, $d, $e y $f y muéstralas
-        usando la función var_dump(<datos>).
+        usando la función var_dump().
     </p>
     <?php
         $a = "0";
@@ -173,23 +182,32 @@
         $e = ($a AND $c);
         $f = ($a XOR $b);
 
-        echo '<span>$a = </span>'; var_dump((bool)$a); echo '<br>';
-        echo '<span>$b = </span>'; var_dump((bool)$b); echo '<br>';
-        echo '<span>$c = </span>'; var_dump($c); echo '<br>';
-        echo '<span>$d = </span>'; var_dump($d); echo '<br>';
-        echo '<span>$e = </span>'; var_dump($e); echo '<br>';
-        echo '<span>$e = </span>'; var_dump($f); echo '<br>';
-    ?>
+        function dumpVar($nombre, $valor) {
+            ob_start();                 // Inicia buffer
+            var_dump((bool)$valor);           // Genera la salida de var_dump
+            $resultado = ob_get_clean(); // Captura en string
+            echo "<p>\$$nombre = $resultado</p>";
+        }
+
+        // Mostrar todas las variables
+        dumpVar("a", $a);
+        dumpVar("b", $b);
+        dumpVar("c", $c);
+        dumpVar("d", $d);
+        dumpVar("e", $e);
+        dumpVar("f", $f);
+        ?>
     <p>
         Después investiga una función de PHP que permita transformar el valor booleano de $c y $e
         en uno que se pueda mostrar con un echo
     </p>
 
     <?php
-        echo '<span>$c = </span>'; echo var_export($c); echo '<br>';
-        echo '<span>$e = </span>'; echo var_export($e); echo '<br>';
+        echo '<p>$e = ' . var_export($c, true) . '</p>';
+        echo '<p>$e = ' . var_export($e, true) . '</p>';
+
     ?>
-    <hr>
+    <hr />
 
     <h2>Ejercicio 7</h2>
     <p>Usando la variable predefinida $_SERVER, determina lo siguiente:</p>
@@ -207,9 +225,11 @@
 
         echo '</ul>';
     ?>
-    
 
-
+    <p>
+        <a href="https://validator.w3.org/check?uri=referer"><img
+        src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+    </p>
 
 </body>
 </html>
